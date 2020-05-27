2458610f3a97c6685261939904248ddba7d66081
daniiloorzhak <daniiloorzhak@gmail.com>
2020-05-27 16:13:24 +0700

    third commit

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int fib_rec(int n)
 int main()
 {
     printf("%d\n", fib_rec(0));
-	printf("%d\n", fib_rec(1));
+    printf("%d\n", fib_rec(1));
+    printf("%d\n", fib_rec(10));
     return 0;
 }
