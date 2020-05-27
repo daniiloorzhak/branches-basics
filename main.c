8112781c2da5c793967b6680adc4b4f4832dab8d
daniiloorzhak <daniiloorzhak@gmail.com>
2020-05-27 13:53:15 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fib_rec(int n)
+{
+    if(n < 0)
+        return -1;
+    if (n == 1 || n == 0)
+        return 1;
+    return fib_rec(n - 1) + fib_rec(n - 2);
+}
+
+int main()
+{
+    printf("%d\n", fib_rec(0));
+    return 0;
+}
